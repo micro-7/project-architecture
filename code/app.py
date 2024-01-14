a4ec4f9dc4714b70a9c2da19babf7527a9951048
mihir-tandon <mihirtandon25@gmail.com>
2024-01-14 13:35:04 +0530

    database

--- code/app.py
+++ code/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template #use fapp
+from flask import Flask, render_template, redirect, flash, request #use fapp
+from database import User, add_to_db, open_db
 
 app = Flask(__name__)
+app.secret_key = 'thisissupersecretkeyfornoone'
 
 @app.route('/')
 def index():
@@ -8,10 +10,28 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST']) # use froute
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        print("Email =>", email)
+        print("Password =>", password)
+        #logic
     return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST']) # use froute
 def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        cpassword = request.form.get('cpassword')
+        print(username, email, password, cpassword)
+        #logic
+        if len(username) == 0 or len(email) == 0 or len(password) == 0 or len(cpassword) == 0:
+            flash("All fields are required", 'danger')
+            return redirect('/register')
+        user = User(username=username, email=email, password=password)
+        add_to_db(user)
     return render_template('register.html')
 
 if __name__ == '__main__':
